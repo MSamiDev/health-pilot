6420250693b38f89d3fb4e94a5259cc937993c3d
Pranshu321 <pranshujain0111@gmail.com>
2023-03-25 20:02:59 +0530

    Added the features of our web app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import "./assets/scss/main.scss";
 import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
 import ChatBot from "./components/chatBot/App";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Dashboard from "./components/Dashboard";
+import UserForm from "./components/UserForm";
+import FunBot from "./components/funBot";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Home />} />
-        <Route path="/contact" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/chatbot" element={<ChatBot />} />
-        <Route path="/funbot" element={<Home />} />
-        <Route path="/dashboard" element={<Home />} />
-        <Route path="/userform" element={<Home />} />
+        <Route path="/funbot" element={<FunBot />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/userform" element={<UserForm />} />
       </Routes>
     </div>
   );
